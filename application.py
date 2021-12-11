5d2755a55b1fb46ab5c070589061789b04db7751
Nagesh Panchal <nagesh.panchal@entercoms.com>
2021-12-11 16:47:57 +0530

    ui changes

--- application.py
+++ application.py
@@ -16,47 +16,66 @@ def index():
 
 @application.route('/students', methods=['POST', 'GET', 'PUT', 'DELETE'])
 def students():
+    result = {"success": False}
+
     if request.method == 'POST':
-        data = json.loads(request.data)
-        new_student = StudentsModel(name=data['name'], contact_number=data['contactNumber'], email_id=data['emailId'])
-        db.session.add(new_student)
-        db.session.commit()
-
-        return jsonify({"success": True})
+        try:
+            data = json.loads(request.data)
+            new_student = StudentsModel(name=data['name'], contact_number=data['contactNumber'], email_id=data['emailId'])
+            db.session.add(new_student)
+            db.session.commit()
+            result = {"success": True}
+        except Exception as e:
+            print(e)
+            result = {"success": False}
+        finally:
+            return jsonify(result)
     elif request.method == 'GET':
-        students = StudentsModel.query.all()
-        results = [
-            {
-                "name": student.name,
-                "contactNumber": student.contactNumber,
-                "emailId": student.emailId,
-                "id": student.id
-            } for student in students]
-
+        try:
+            students = StudentsModel.query.all()
+            results = [
+                {
+                    "name": student.name,
+                    "contactNumber": student.contactNumber,
+                    "emailId": student.emailId,
+                    "id": student.id
+                } for student in students]
+        except Exception as e:
+            print(e)
         return jsonify({"students": results})
 
     elif request.method == 'PUT':
-        data = json.loads(request.data)
-        student = StudentsModel.query.get_or_404(data['id'])
-        if data['name'] and student.name != data['name']:
-            student.name = data['name']
-        if data['contactNumber'] and student.contactNumber != data['contactNumber']:
-            student.contactNumber = data['contactNumber']
-        if data['emailId'] and student.emailId != data['emailId']:
-            student.emailId = data['emailId']
+        try:
+            data = json.loads(request.data)
+            student = StudentsModel.query.get_or_404(data['id'])
+            if data['name'] and student.name != data['name']:
+                student.name = data['name']
+            if data['contactNumber'] and student.contactNumber != data['contactNumber']:
+                student.contactNumber = data['contactNumber']
+            if data['emailId'] and student.emailId != data['emailId']:
+                student.emailId = data['emailId']
 
-        db.session.add(student)
-        db.session.commit()
-        return jsonify({"success": True})
+            db.session.add(student)
+            db.session.commit()
+            result = {"success": True}
+        except Exception as e:
+            print(e)
+            result = {"success": False}
+        finally:
+            return jsonify(result)
 
     elif request.method == 'DELETE':
-        # data = request.get_json()
-        data = json.loads(request.data)
-        student = StudentsModel.query.get_or_404(data['id'])
-        db.session.delete(student)
-        db.session.commit()
-        return jsonify({"success": True})
-
+        try:
+            data = json.loads(request.data)
+            student = StudentsModel.query.get_or_404(data['id'])
+            db.session.delete(student)
+            db.session.commit()
+            result = {"success": True}
+        except Exception as e:
+            print(e)
+            result = {"success": False}
+        finally:
+            return jsonify(result)
 
 application.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://python:python123@database-1.ckrojnpkqucm.us-east-1.rds.amazonaws.com:5432/student"
 db = SQLAlchemy(application)
