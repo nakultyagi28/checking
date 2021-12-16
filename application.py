737e876304aab23d88cdc378b4dc87362d23832f
Nagesh Panchal <nakultyagi27@gmail.com>
2021-12-17 00:07:10 +0530

    new

--- application.py
+++ application.py
@@ -14,6 +14,7 @@ def index():
     return render_template('app.html')
 
 
+
 @application.route('/students', methods=['POST', 'GET', 'PUT', 'DELETE'])
 def students():
     result = {"success": False}
